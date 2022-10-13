1354a4366e4006c3bad209efb1fe6aa5373b3c26
thiagohenriquedev <thiagohenriquedev>
2022-10-13 15:02:25 -0300

    strings

---
+++ strings.rb
@@ -0,0 +1,30 @@
+x = "Thiago"
+y = 'Rails'
+
+puts x
+puts x.class
+puts y
+puts y.class
+
+a = "Curso"
+b = "Rails"
+
+puts a << b 
+puts a + b
+
+x = "curso"
+puts x.object_id
+x = x + "rails"
+puts x
+puts x.object_id
+##############
+q = "curso de "
+puts q.object_id
+q << "rails"
+puts q
+puts q.object_id
+
+
+h = "Thiago #{1+1} Henrique #{q}"
+puts h
+
